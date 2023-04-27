0fbcc30fd868fc0313811603b10f01b62b78350c
Luiz <luizhc2309@hotmail.com>
2023-04-26 23:57:16 -0300

    start de projeto

---
+++ src/Summary.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Summary() {
+  return (
+    <div className="Summary">
+      
+    </div>
+  );
+}
+
+export default Summary;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Summary from './Summary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Summary />
   </React.StrictMode>
 );
 
