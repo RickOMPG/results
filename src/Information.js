6901bab0c9de57a02fe2698cecbd875654be4935
Luiz <luizhc2309@hotmail.com>
2023-04-28 00:57:58 -0300

    Squashed commit of the following:
    commit 7713fbaac9e1222552f99a9a978ef6fce9508c87
    Author: Luiz <luizhc2309@hotmail.com>
    Date:   Fri Apr 28 00:57:31 2023 -0300
    
        correções
    
    commit 25442dd3e438da3da2d5ac9d55c6cfc33d57bcd9
    Author: Luiz <luizhc2309@hotmail.com>
    Date:   Thu Apr 27 00:48:44 2023 -0300
    
        criação de classnames e itens

---
+++ src/Information.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classNames from "classnames";
+
+function Information(props) {
+
+    const SummaryStyle = classNames ( "Summary-item", {
+        "Summary--orange": props.color === "orange",
+        "Summary--yellow": props.color === "yellow",
+        "Summary--green": props.color === "green",
+        "Summary--blue": props.color === "blue",
+    })
+
+    const { summaryIcon, summaryText, summaryNote } = props;
+
+
+    return (
+        <div className={SummaryStyle}>
+            <div className="Summary-item__both">
+                <div className='Summary-item__icon'>{summaryIcon}</div>
+                <p className='Summary-item__text'>{summaryText}</p>
+            </div>
+        <p className='Summary-item__note'>{summaryNote}/100</p>
+        </div>
+    );
+}
+
+export default Information;
--- src/Summary.js
+++ src/Summary.js
@@ -1,11 +1,38 @@
 import './Summary.css';
+import React from "react";
+import Information from "./Information"
 
 function Summary() {
+
   return (
     <div className="Summary">
-      
+      <Information
+        color='orange'
+        summaryIcon='Icon'
+        summaryText='Reaction'
+        summaryNote={80}
+      />
+      <Information
+        color='yellow'
+        summaryIcon='Icon'
+        summaryText='Memory'
+        summaryNote={92}
+      />
+      <Information
+        color='green'  
+        summaryIcon='Icon'
+        summaryText='Verbal'
+        summaryNote={61}
+      />
+      <Information
+        color='blue'
+        summaryIcon='Icon'
+        summaryText='Visual'
+        summaryNote={72}
+      />
     </div>
   );
 }
+
 
 export default Summary;
